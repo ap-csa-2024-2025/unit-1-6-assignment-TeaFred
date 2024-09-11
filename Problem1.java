4f715bef2cfb566a99a02d55341edf84fdac4ba5
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-09-11 11:06:11 -0700

    Problem1 Done

--- Problem1.java
+++ Problem1.java
@@ -2,6 +2,14 @@ public class Problem1
 {
   public static void main(String[] args)
   {
-    // insert code here
+    double x = 5/20;
+    System.out.println(x);
+    int numerator = 5;
+    int denominator = 20;
+    double realx = (double)numerator/denominator;
+    System.out.println("The numerator is " + numerator);
+    System.out.println("The denominator is " + denominator);
+    System.out.println("The decimal value is " + realx);
+
   }
 }
