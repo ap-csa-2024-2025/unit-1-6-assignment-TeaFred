0b08cfca3b242ae932aa52bc975c680f1eb21507
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-09-12 11:22:16 -0700

    yolo

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,15 @@
-
+public class Problem2
+{
+    public static void main(String[] args)
+    {
+        double num1 = 57.3934;
+        double num2 = 22.5211;
+        int round1 = (int) (num1 + 0.5);
+        int round2 = (int) (num2 + 0.5);
+        int sum = round1 + round2;
+        System.out.println("Two original numbers: ");
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println("The answer is " + round1 + " + " + round2 + " = " + sum);
+    }
+}
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,28 @@
+public class Problem3
+{
+    public static void main(String[] args)
+    {
+        double decimalNum = 67.3424;
+        int semiFinalNum;
+        int intNum = (int) decimalNum;
+        double simplifiedNum = decimalNum - intNum;
+        //.3424 is left
+        while (semiFinalNum == null)
+        {
+            simplifiedNum *=10;
+            try
+            {
+                semiFinalNum = simplifiedNum;
+            }
+            catch (ArithmeticException e);
+        }
 
+        System.out.println(semiFinalNum);;
+        
+        
+        int finalNum = semiFinalNum/10;
+        System.out.println(finalNum/100);
+        System.out.println(finalNum/10%10);
+        System.out.println(finalNum%10);
+    }
+}
